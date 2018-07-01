8656d1ff3927f93d5bcf0811cd734f8dc55552df
Colin Targonski <ctargon@g.clemson.edu>
2018-07-01 17:31:32 -0400

    update deployment script

--- deploy-gene-oracle.sh
+++ deploy-gene-oracle.sh
@@ -2,6 +2,11 @@
 echo "Instantiating pod..."
 kubctl create -f gene-oracle-pod.yaml
 
+sleep 5
+kubectl get pods
+
+echo "IF YOU DO NOT SEE YOUR POD NAME, KILL THIS SCRIPT"
+
 echo "Copying subset data..."
 kubectl cp /home/eceftl2/workspace/SciDAS/gene-oracle/data/ deepgtex-prp/gene-oracle:/gene-oracle
 
