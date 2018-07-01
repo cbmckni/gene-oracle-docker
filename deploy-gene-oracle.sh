757261969264d2bec569cb3661834045b896948d
Colin Targonski <ctargon@g.clemson.edu>
2018-07-01 17:16:36 -0400

    change order of copying

--- deploy-gene-oracle.sh
+++ deploy-gene-oracle.sh
@@ -2,8 +2,9 @@
 echo "Instantiating pod..."
 kubctl create -f gene-oracle-pod.yaml
 
+echo "Copying subset data..."
+kubectl cp /home/eceftl2/workspace/SciDAS/gene-oracle/data/ deepgtex-prp/gene-oracle:/gene-oracle
+
 echo "Copying data..."
 kubectl cp /home/eceftl2/workspace/SciDAS/gene-oracle/data/ deepgtex-prp/gene-oracle:/
 
-echo "Copying subset data..."
-kubectl cp /home/eceftl2/workspace/SciDAS/gene-oracle/data/ deepgtex-prp/gene-oracle:/gene-oracle
