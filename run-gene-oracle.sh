739aa37b87187181e624e02409b87a6cab86fff9
Colin Targonski <ctargon@g.clemson.edu>
2018-07-01 17:19:05 -0400

    cmd line arg addition

--- run-gene-oracle.sh
+++ run-gene-oracle.sh
@@ -8,13 +8,16 @@
 # --config ./models/net_config.json \
 # --out_file /data/logs/hallmark_classify_kfold10.log
 
+echo "Running gene oracle algorithm on subset: "
+echo $1
+
 python scripts/gene-oracle.py  \
 	--dataset /data/float_data/gtex_gct_data_float_v7.npy \
 	--gene_list /data/gene_lists/gtex_gene_list_v7.npy \
 	--sample_json /data/class_counts/gtex_tissue_count_v7.json \
 	--config ./models/net_config.json \
 	--subset_list ./subsets/hallmark_experiments.txt \
-	--set hallmark_hedgehog_signaling \
+	--set $1 \
 	--num_genes 36 \
-	--log_dir ./logs/hallmark_hedgehog_signaling
+	--log_dir ./logs/$1
 
